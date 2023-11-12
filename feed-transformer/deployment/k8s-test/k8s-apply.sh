e20a8c838f2bb10df51f3c080c3c71760c2c0cda
vladislav-chunikhin <vladislav.chunikhin.95@gmail.com>
2023-11-12 17:17:34 +0700

    added k8s test for feed transformer service

--- feed-transformer/deployment/k8s-test/k8s-apply.sh
+++ feed-transformer/deployment/k8s-test/k8s-apply.sh
@@ -3,26 +3,59 @@
 # Declare environment variables
 export RABBITMQ_USERNAME=${RABBITMQ_USERNAME:-vlad}
 export RABBITMQ_PASSWORD=${RABBITMQ_PASSWORD:-sport}
-export TEST_IMAGE=${TEST_IMAGE:-sport-news/feed-service:local}
+export MONGO_USERNAME=${MONGO_USERNAME:-vlad}
+export MONGO_PASSWORD=${MONGO_PASSWORD:-sport}
+export TEST_IMAGE=${TEST_IMAGE:-sport-news/feed-transformer:local}
 export RELATIVE_PATH=${RELATIVE_PATH:-}
 
 # Create Namespace
-kubectl apply -f "${RELATIVE_PATH}feed-service-namespace.yaml"
+kubectl apply -f "${RELATIVE_PATH}feed-transformer-namespace.yaml"
 
 # Prepare ConfigMap YAML with variable substitution
 envsubst < "${RELATIVE_PATH}rabbitmq-configmap.yaml" | kubectl apply -f -
+envsubst < "${RELATIVE_PATH}mongodb-configmap.yaml" | kubectl apply -f -
 
 # Deploy RabbitMQ
 kubectl apply -f "${RELATIVE_PATH}rabbitmq-deployment.yaml"
 kubectl apply -f "${RELATIVE_PATH}rabbitmq-service.yaml"
 
+# Deploy MongoDB
+kubectl apply -f "${RELATIVE_PATH}mongodb-deployment.yaml"
+kubectl apply -f "${RELATIVE_PATH}mongodb-service.yaml"
+
+# Deploy Redis
+kubectl apply -f "${RELATIVE_PATH}redis-deployment.yaml"
+kubectl apply -f "${RELATIVE_PATH}redis-service.yaml"
+
 # Wait for RabbitMQ to be ready
 echo "Waiting for RabbitMQ to become ready..."
-if kubectl wait --namespace feed-service --for=condition=ready pod -l app=rabbitmq --timeout=180s; then
+if kubectl wait --namespace feed-transformer --for=condition=ready pod -l app=rabbitmq --timeout=180s; then
     echo "RabbitMQ is ready."
-    # Deploy Feed Fetcher
-    envsubst < "${RELATIVE_PATH}feed-service-deployment.yaml" | kubectl apply -f -
-    kubectl apply -f "${RELATIVE_PATH}feed-service-service.yaml"
+
+    # Wait for MongoDB to be ready
+    echo "Waiting for MongoDB to become ready..."
+    if kubectl wait --namespace feed-transformer --for=condition=ready pod -l app=mongodb --timeout=180s; then
+        echo "MongoDB is ready."
+
+        # Wait for Redis to be ready
+        echo "Waiting for Redis to become ready..."
+        if kubectl wait --namespace feed-transformer --for=condition=ready pod -l app=redis --timeout=180s; then
+
+            # Deploy Feed transformer
+            envsubst < "${RELATIVE_PATH}feed-transformer-deployment.yaml" | kubectl apply -f -
+            kubectl apply -f "${RELATIVE_PATH}feed-transformer-service.yaml"
+
+        else
+            echo "Redis is not ready within the timeout period. Exiting..."
+            ./k8s-delete.sh
+            exit 1
+        fi
+    else
+        echo "MongoDB is not ready within the timeout period. Exiting..."
+        ./k8s-delete.sh
+        exit 1
+    fi
+
 else
     echo "RabbitMQ is not ready within the timeout period. Exiting..."
     ./k8s-delete.sh
--- feed-transformer/deployment/k8s-test/k8s-delete.sh
+++ feed-transformer/deployment/k8s-test/k8s-delete.sh
@@ -2,11 +2,11 @@
 
 export RELATIVE_PATH=${RELATIVE_PATH:-}
 
-# Delete Feed Fetcher Service
-kubectl delete -f "${RELATIVE_PATH}feed-service-service.yaml"
+# Delete Feed Transformer Service
+kubectl delete -f "${RELATIVE_PATH}feed-transformer-service.yaml"
 
-# Delete Feed Fetcher Deployment
-kubectl delete -f "${RELATIVE_PATH}feed-service-deployment.yaml"
+# Delete Feed Transformer Deployment
+kubectl delete -f "${RELATIVE_PATH}feed-transformer-deployment.yaml"
 
 # Delete RabbitMQ Service
 kubectl delete -f "${RELATIVE_PATH}rabbitmq-service.yaml"
@@ -17,5 +17,20 @@ kubectl delete -f "${RELATIVE_PATH}rabbitmq-deployment.yaml"
 # Delete RabbitMQ ConfigMap
 kubectl delete -f "${RELATIVE_PATH}rabbitmq-configmap.yaml"
 
-# Delete Feed Fetcher Namespace
-kubectl delete -f "${RELATIVE_PATH}feed-service-namespace.yaml"
+# Delete MongoDB Service
+kubectl delete -f "${RELATIVE_PATH}mongodb-service.yaml"
+
+# Delete MongoDB Deployment
+kubectl delete -f "${RELATIVE_PATH}mongodb-deployment.yaml"
+
+# Delete MongoDB ConfigMap
+kubectl delete -f "${RELATIVE_PATH}mongodb-configmap.yaml"
+
+# Delete Redis Service
+kubectl delete -f "${RELATIVE_PATH}redis-service.yaml"
+
+# Delete Redis Deployment
+kubectl delete -f "${RELATIVE_PATH}redis-deployment.yaml"
+
+# Delete Feed Transformer Namespace
+kubectl delete -f "${RELATIVE_PATH}feed-transformer-namespace.yaml"
